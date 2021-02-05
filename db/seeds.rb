1ac1b883a7f48a045601917a059cf69897419d68
Annette Wong <nettecodes@gmail.com>
2021-02-05 16:17:01 +0000

    1st commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Kahlua")
+Ingredient.create(name: "Coffee")
+Ingredient.create(name: "Amaretto")
+Ingredient.create(name: "Irish cream liquer")
+Ingredient.create(name: "Cream")
+Ingredient.create(name: "Milk")
+Ingredient.create(name: "Passoa")
+Ingredient.create(name: "Vanilla simple syrup")
+Ingredient.create(name: "Passion fruit puree")
+Ingredient.create(name: "Lime juice")
+Ingredient.create(name: "Sparkling wine")
+Ingredient.create(name: "Passion fruit")
+
